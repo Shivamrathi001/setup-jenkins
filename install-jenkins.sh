157fc84ddca1d565a966112b5c758802d47992b7
Shivamrathi001 <106222099+Shivamrathi001@users.noreply.github.com>
2023-04-05 10:01:29 +0530

    Update install-jenkins.sh

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt install openjdk-11-jdk -y
 java --version
-wget -p -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt install jenkins -y
